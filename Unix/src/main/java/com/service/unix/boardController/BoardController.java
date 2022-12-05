703d63fa6b60909cd0450b099459ee5f3bff166a
Shye-Seo <tjtmdgp95@gmail.com>
2022-12-05 16:54:50 +0900

    페이징 total 수정

--- Unix/src/main/java/com/service/unix/boardController/BoardController.java
+++ Unix/src/main/java/com/service/unix/boardController/BoardController.java
@@ -292,6 +292,8 @@ public class BoardController
 		}else {
 			pagingvo.setSql(sql);
 			total = service.library_count(pagingvo);
+		}if(total == 0) {
+			total = 1;
 		}
 		
 		modelMap.addAttribute("total", total);
